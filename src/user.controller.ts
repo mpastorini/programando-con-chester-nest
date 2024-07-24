a0d55c48b28c07389fae041bbe18c958e4cc00e4
Esteban <estebanavella1@gmail.com>
2024-07-24 19:46:05 -0300

    clase nro2, basic CRUD

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,22 +1,58 @@
-import {Controller, Get, Req, UseFilters, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UseFilters,
+  Query,
+  Post,
+  Body,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { query, Request } from 'express';
+
+const user1 = {
+  id: 1,
+  name: 'Carlos',
+  lastName: 'Arteaga',
+};
+const user2 = {
+  id: 2,
+  name: 'pepe',
+  lastName: 'grillo',
+};
+const users = [user1, user2];
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    findAll(@Query() query: any): Array<Object> {
-        const user1 = {
-            name: 'Carlos',
-            lastName: 'Arteaga'
+  @Get()
+  findAll(@Query() query: any): Array<Object> {
+    const filterByName = (user) => user.name === query.name;
+    const filteredUsers = query.name ? users.filter(filterByName) : users;
+    return filteredUsers;
+  }
 
-        }
-        const user2 ={
-            name: 'pepe',
-            lastName: 'grillo'
-        }
-        const users= [user1,user2]
-        const filteredUser = users.filter(user => user.name === query.name || user.lastName === query.lastName )
-       
-        return filteredUser
+  // @Post decorators
+  @Post()
+  create(@Body() body: any) {
+    users.push(body);
+
+    return 'la persona se creo correctamente';
+  }
+  @Patch(':chompiras')
+  update(@Param('chompiras') id, @Body() body: any) {
+    console.log('id de usuario', id);
+
+    const findById = function (user) {
+      return user.id === parseInt(id);
+    };
+    const userIndex = users.findIndex(findById);
+
+    console.log(userIndex);
+    users[userIndex] = body;
+    if (userIndex === -1) {
+      return 'no se encontro el user';
     }
+    return 'el user se actualizo correctamente';
+  }
 }
