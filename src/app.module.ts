4cffc2e25d514aa6119aab2c522d64efb658c1e8
Esteban <estebanavella1@gmail.com>
2024-08-02 18:39:08 -0300

    docker-compose added and configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { PizzasController } from './pizzas/pizzas.controller';
 import { UsersController } from './user.controller';
 import { PizzaModule } from './modules/pizza/pizza.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pizza } from './modules/pizza/pizza.entity';
+
 
 @Module({
-  imports: [PizzaModule],
-  controllers: [AppController, PizzasController,UsersController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'esteban',
+      password: 'qwe123',
+      database: 'pizza',
+      entities: [Pizza],
+      synchronize: true,
+    }), PizzaModule],
+  controllers: [AppController, PizzasController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
