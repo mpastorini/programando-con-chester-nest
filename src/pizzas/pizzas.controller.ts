912e895b6469480f603b72e18b23b9ede5c14f86
Esteban Avella <estebanavella1@gmail.com>
2024-08-08 19:50:35 -0300

    all methods of pizzacontroller was updated

--- src/pizzas/pizzas.controller.ts
+++ src/pizzas/pizzas.controller.ts
@@ -10,9 +10,7 @@ import {
   Req,
   Body,
 } from '@nestjs/common';
-import { Request } from 'express';
 import { Pizza } from 'src/modules/pizza/pizza.entity';
-import pizzasList from './pizzasList';
 import { PizzaService } from '../modules/pizza/pizza.service';
 
 @Controller('pizzas')
@@ -23,21 +21,18 @@ export class PizzasController {
   async findAll(): Promise<Partial<Pizza>[]> {
     return this.pizzaService.findAll();
   }
+  @Get(":id")
+  async findOne(@Param("id") id): Promise<Pizza>{
+    return this.pizzaService.findOneById(id);
+  }
+  
   @Post()
   async create(@Body() body: any) {
     return this.pizzaService.createPizza(body);
   }
   @Patch(':id')
   async update(@Param('id') id, @Body() body: any) {
-    console.log('id de usuario es ', id);
-
-    const findById = (pizza) => pizza.id === parseInt(id);
-    const pizzaIndex = pizzasList.findIndex(findById);
-
-    pizzasList[pizzaIndex] = body;
-    if (pizzaIndex === -1) {
-      return 'Pizza not found whit id:' + id;
-    }
+    return this.pizzaService.updatePizza(body, id );
   }
   @Delete(':id')
   async remove(@Param('id') id: string) {
