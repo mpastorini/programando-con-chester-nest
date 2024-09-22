7928336093a1bb1e1adcbdea1394b520aad835d7
Esteban Avella <estebanavella1@gmail.com>
2024-09-22 17:41:01 -0300

    test in auth mocked repository

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>
+
+  const mockUserRepository = {
+    // Métodos del repositorio mockeados
+    findOne: jest.fn(),
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService,
+      {
+        provide: getRepositoryToken(User), // Token para el UserRepository
+        useValue: mockUserRepository, // Mock de los métodos del repositorio
+      },
+    ]
     }).compile();
 
     service = module.get<UsersService>(UsersService);
