6b317ba8fbc5062843dbc90cfbae4b107204a9b7
Esteban <estebanavella1@gmail.com>
2024-10-01 11:04:45 -0300

    auth.spec configurated

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,15 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let jwtService: JwtService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService, // Mock del AuthService
+          useValue: {
+            singIn: jest
+              .fn()
+              .mockResolvedValue({ access_token: 'mockedJwtToken' }), // Mock de la función singIn
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('mockedJwtToken'),
+          },
+        },
+        {
+          provide: UsersService, // Mockea UsersService si es necesario
+          useValue: {
+            findOne: jest.fn().mockResolvedValue({
+              username: 'testUser',
+              userId: 1,
+              password: 'hashedPassword', // Simula un hash de contraseña
+            }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    jwtService = module.get<JwtService>(JwtService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let jwtService: JwtService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn().mockReturnValue('mockedJwtToken'),
+          },
+        },
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue({
+              id: 1,
+              username: 'testUser',
+              password: await bcrypt.hash('password', 10),
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  it('should return a JWT token', async () => {
+    const username = 'testUser';
+    const password = 'password';
+
+    const result = await service.singIn(username, password);
+
+    expect(result).toEqual({ acces_token: 'mockedJwtToken' });
+    expect(usersService.findOne).toHaveBeenCalledWith(username);
+    expect(jwtService.signAsync).toHaveBeenCalledWith({
+      username: 'testUser',
+      sub: 1,
+    });
+  });
+
+  it('should throw an UnauthorizedException if credentials are invalid', async () => {
+    jest
+      .spyOn(bcrypt, 'compare')
+      .mockImplementation((plainText: string, hashed: string) => {
+        return Promise.resolve(false);
+      });
+    const username = 'testUser';
+    const password = 'wrongPassword';
+
+    await expect(service.singIn(username, password)).rejects.toThrow(
+      UnauthorizedException
+    );
   });
 });
